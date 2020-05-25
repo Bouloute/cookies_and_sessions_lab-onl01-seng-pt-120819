03612acfe337a44d33fdd2e2f6e905ee00df6abc
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-05-25 21:31:02 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,10 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
-    # Get the item from the path
-    #binding.pry
-   
+    
     # Load the cart from the session, or create a new empty cart.
     #cart = session[:cart] || [] # This is same as helper_method "cart"
-    cart << params[:product]
+    cart << params[:product]# Get the item from the path
    
     # Save the cart in the session.
     #session[:cart] = cart #no need for this as cart renders the session
