c2390fe464aec535fb7016716c76b87a54d1af09
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-05-25 21:06:50 +0000

    Automatically backed up by Learn

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ActionController::Base
+  def index
+  end
+
+  def add_to_cart
+    # Get the item from the path
+    @item = Product.find(params[:id])
+   
+    # Load the cart from the session, or create a new empty cart.
+    cart = session[:cart] || []
+    cart << @item.id
+   
+    # Save the cart in the session.
+    session[:cart] = cart
+  end
+  
+end
